d089435728ec2ee147e84ed821dea6cf91fb7d03
nguyenhoangkhang-2010 <nguyenhoangkhang.241105@gmail.com>
2026-07-10 13:54:52 +0700

    feat(repositories): add video repository

--- backend/app/repositories/__init__.py
+++ backend/app/repositories/__init__.py
@@ -1,7 +1,9 @@
 from app.repositories.base import BaseRepository
 from app.repositories.user import UserRepository
+from app.repositories.video import VideoRepository
 
 __all__ = [
     "BaseRepository",
     "UserRepository",
+    "VideoRepository",
 ]
