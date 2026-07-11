03b6fddd807011a7851a52ed80984ca485787e42
nguyenhoangkhang-2010 <nguyenhoangkhang.241105@gmail.com>
2026-07-11 21:56:58 +0700

    feat(services): validate duplicate user information

--- backend/app/services/auth_service.py
+++ backend/app/services/auth_service.py
@@ -1,5 +1,8 @@
 from app.repositories.user import UserRepository
-
+from app.core.exceptions import UserAlreadyExistsError
+from app.core.security import hash_password
+from app.models.user import User
+from app.schemas.user import UserCreate
 
 class AuthService:
     """Business logic for authentication."""
@@ -8,3 +11,23 @@ class AuthService:
         user_repository: UserRepository,
     ) -> None:
         self.user_repository = user_repository
+        
+    def register(
+        self,
+        user_data: UserCreate,
+    ) -> User:
+        """Register a new user."""
+        if self.user_repository.exists_by_email(
+            user_data.email,
+        ):
+            raise UserAlreadyExistsError(
+                "Email already exists.",
+            )
+
+        if self.user_repository.exists_by_username(
+            user_data.username,
+        ):
+            raise UserAlreadyExistsError(
+                "Username already exists.",
+            )
+        raise NotImplementedError
